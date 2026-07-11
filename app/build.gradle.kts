f2713742f7bf83fe49025e2b96b6cad0d257af2c
hehenya <hehehuidashi@outlook.com>
2026-07-11 11:53:58 +0800

    修复深色模式，支持自定义气泡圆角和透明度

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -125,6 +125,8 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
 }
 
 configurations.all {
